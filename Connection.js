0e2c581704e58d8853c365e56f84807ce4eb7050
aswinram.92@gmail.com <aswinram.92@gmail.com>
2017-06-19 17:52:23 -0400

    Updated

--- Connection.js
+++ Connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var connectionPool = mysql.createPool({
     connectionLimit: 100,
-    host: 'localhost',
+    host: 'edissdatabase.ctpoqk8h72ri.us-east-1.rds.amazonaws.com:3306',
     user: 'root',
     password: 'password',
     database: 'edissproject2',
