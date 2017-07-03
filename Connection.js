10a72b05a525f824c8625685db5a1f4df169d195
aswinram.92@gmail.com <aswinram.92@gmail.com>
2017-07-03 14:33:18 -0400

    Project3-Connection Limit Increased

--- Connection.js
+++ Connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connectionPool = mysql.createPool({
-    connectionLimit: 100,
+    connectionLimit: 500,
     host: 'edissdatabase.ctpoqk8h72ri.us-east-1.rds.amazonaws.com',
     port:3306,
     user: 'root',
